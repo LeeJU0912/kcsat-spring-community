dffe7a81ba8140f568ba484c3d154af454edf748
LJU <thirdspirit@naver.com>
2025-04-10 02:27:50 +0900

    refactor(PostRepository): fix N+1 issue
    use fetch join when request post details.

--- src/main/java/hpclab/kcsatspringcommunity/community/repository/PostRepository.java
+++ src/main/java/hpclab/kcsatspringcommunity/community/repository/PostRepository.java
@@ -5,13 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
-    // N + 1 방지 FETCH JOIN
-    @Query("SELECT p FROM Post p JOIN FETCH p.comment")
-    Page<Post> findAllWithComments(Pageable pageable);
+    /**
+     * 게시글 ID에 대해 게시글 상세 정보를 가져웁니다.
+     *
+     * N+1 처리 Fetch join 적용으로 post 가져올 때 comment도 같이 가져오도록 합니다.
+     * @param postId 게시글 ID
+     * @return 게시글 상세 정보
+     */
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.comment WHERE p.pId = :postId")
+    Post findByIdWithComments(@Param("postId") Long postId);
 }
