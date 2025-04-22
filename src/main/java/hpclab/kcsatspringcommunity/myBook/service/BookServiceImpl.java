1bd30009df80bdcc5f324fca957acd3476bfa287
LeeJU <thirdspirit@naver.com>
2025-04-23 00:59:05 +0900

    Refactor findBook method to return Book directly.
    Simplified the findBook method by removing the conversion to BookResponseForm. Eliminated related helper methods for DTO conversion to streamline the code and improve maintainability.

--- src/main/java/hpclab/kcsatspringcommunity/myBook/service/BookServiceImpl.java
+++ src/main/java/hpclab/kcsatspringcommunity/myBook/service/BookServiceImpl.java
@@ -26,39 +26,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public BookResponseForm findBook(String userEmail) {
-        return BookToBookResponseForm(bookRepository.findByEmail(userEmail).orElseThrow(() -> new IllegalArgumentException("Member not found")));
-    }
-
-    /**
-     * Book -> BookDTO 형태로 변환하는 메서드입니다.
-     * @param book 순수한 Book 질문 객체
-     * @return BookDTO 객체로 변환하여 데이터 정합성을 보장합니다.
-     */
-    private BookResponseForm BookToBookResponseForm(Book book) {
-        return BookResponseForm.builder()
-                .question(book.getBookQuestions().stream()
-                        .map(BookQuestion::getQuestion)
-                        .map(this::questionToDto)
-                        .toList())
-                .build();
-    }
-
-    /**
-     * Question -> QuestionDTO 형태로 변환하는 메서드입니다.
-     * @param question 순수한 Question 질문 객체
-     * @return QuestionDTO 객체로 변환하여 데이터 정합성을 보장합니다.
-     */
-    private QuestionResponseForm questionToDto(Question question) {
-
-        return QuestionResponseForm.builder()
-                .qId(question.getId())
-                .questionType(question.getType().getKrName())
-                .title(question.getTitle())
-                .mainText(question.getMainText())
-                .choices(question.getChoices().stream().map(Choice::getChoice).toList())
-                .shareCounter(question.getShareCounter())
-                .createdDate(question.getCreatedDate())
-                .build();
+    public Book findBook(String userEmail) {
+        return bookRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
     }
 }
