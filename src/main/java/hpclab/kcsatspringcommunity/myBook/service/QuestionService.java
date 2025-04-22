db70ba371d64a5dae8630f2a3b255122f4b41bfa
LeeJU <thirdspirit@naver.com>
2025-04-22 21:33:07 +0900

    Add saveQuestion method to QuestionService interface
    Introduced the saveQuestion method to handle saving question entities to the database. This addition provides a way to persist questions and return their generated IDs. Updated Javadoc comments for clear documentation.

--- src/main/java/hpclab/kcsatspringcommunity/myBook/service/QuestionService.java
+++ src/main/java/hpclab/kcsatspringcommunity/myBook/service/QuestionService.java
@@ -8,10 +8,18 @@ import hpclab.kcsatspringcommunity.question.domain.Question;
 public interface QuestionService {
 
     /**
-     * 문제 ID를 이용하여 문제 객체를 가져오는 클래스입니다.
+     * 문제 ID를 이용하여 문제 객체를 가져오는 메서드입니다.
      *
      * @param qId 문제 ID
      * @return 문제 객체를 반환합니다.
      */
     Question getQuestion(Long qId);
+
+    /**
+     * 문제 객체를 DB에 저장하는 메서드입니다.
+     *
+     * @param question 생성된 문제 엔티티 데이터
+     * @return 저장된 question ID를 반환합니다.
+     */
+    Long saveQuestion(Question question);
 }
